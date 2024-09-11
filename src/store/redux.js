186b778d58a7782ce0593005a77499955eab741b
Amisha PRIYA <priyaamisha03@gmail.com>
2024-09-11 22:56:20 +0530

    Never mutate existing state

--- src/store/redux.js
+++ src/store/redux.js
@@ -7,6 +7,8 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
+        // This return value will overwrite the existing state
+        // Thumb rule in react - never mutate the existing state , always override it. Mutating might lead to bugs, unpredictable behaviour, side effects and make debugging difficult
         return {
             ...state,
             counter: state.counter + action.amount
