748ab89f22481fa981cbc1e43c85b33d187ebd96
Amisha PRIYA <priyaamisha03@gmail.com>
2024-09-11 22:08:46 +0530

    Using redux data in react comp via useSelector hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
+import { useSelector } from 'react-redux'; // custom hook provided by react-redux
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
 
+  /**
+   * Function called by react-redux. Current state value is passed to it and we make it return the counter value from the state.
+   * 
+   * When we use this function, react-redux will automatically set up a subscription to redux store for this component. Thus, this
+   * component will be updated/re-executed i.e it will receive the latest value of the store whenever the counter state changes.
+   * 
+   * When this component is unmounted from the DOM, react-redux will automatically clear this subscription for us
+   */
+  const counter = useSelector(state => state.counter);
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
